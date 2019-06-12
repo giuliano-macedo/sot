2e79905cd55b77cac169675fca104d34316e192f
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-06-12 11:28:03 -0400

    WIP na arvore

--- src/web_interface.py
+++ src/web_interface.py
@@ -1,17 +1,32 @@
 from core import FileSystem
 from flask import Flask,jsonify,send_from_directory
 from argparse import ArgumentParser
+
+# text: { name: "Parent node" },
+# 		children: [
+# 			{
+# 				text: { name: "First child" }
+# 			},
+# 			{
+# 				text: { name: "Second child" }
+# 			}
+# 		]
+def gentree(fs):
+	ans={}
+	return ans
 app=Flask(__name__)
 @app.route("/webservice")
 def webservice():
 	try:
 		fs=FileSystem(args.image)
+		tree=gentree(fs)
 		ans={
 			"status":"ok",
 			"bitarray":fs.bitmap.bitstr(),
 			"allocset":list(fs.bitmap.allocblocks),
 			"freeset": list(fs.bitmap.freeblocks),
-			"diskpercentage":fs.bitmap.getPercentageFull()
+			"diskpercentage":fs.bitmap.getPercentageFull(),
+			"tree":tree
 		}
 		del fs
 	except Exception as e:
